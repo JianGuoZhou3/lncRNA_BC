b12c555325ee8aed6691d9ceb729920f83875868
LauraMCE <53834929+LauraMCE@users.noreply.github.com>
2019-10-28 13:49:42 -0600

    Added first module of Transcriptome Pipeline
    First module: Quality control

---
+++ Transcriptome/bin/1_QUALITY_CONTROL_TRANSCRIPTOME.R
@@ -0,0 +1,35 @@
+#Pre-requisites##
+
+#R version 3.6#
+
+##Packages to import files##
+
+install.packages("plyr", "readr") #v. 1.8.4 & v.1.3.1#
+
+##Packages for Quality Control##
+
+install.packages("BiocManager") #v. 3.0#
+
+library(plyr)
+library(readr)
+library(BiocManager)
+
+BiocManager::install("Rqc") #v.1.18.0#
+BiocManager::install("BiocStyle") #v.3.9#
+
+library(BiocStyle)
+library(Rqc)
+
+#Import files to R environment
+setwd("~/lncRNA_BC")
+CMLA = "Transcriptome"
+FASTQFILES = list.files(path=CMLA, pattern = "*.fastqsanger", fullnames = TRUE)
+FASTQFILES
+
+#Quality control for .fastqfiles#
+
+CMLA_QC <- system.file(package="ShortRead", "fastqfiles")
+list.files(CMLA_QC, ".fastqsanger", full.names=TRUE) #Indicates files for QC report#
+QC <- rqcQA(files, workers=1) #Run QC analysis#
+QC_REPORT <- rqcReport(QC) #Print and save QC report#
+browseURL(QC_REPORT) #Visualize QC report#
